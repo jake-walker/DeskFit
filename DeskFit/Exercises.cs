3de4e673d6e6c76437c5fd1e061fb2ff97801a57
Jake Walker <jcbod101@gmail.com>
2018-03-19 21:58:33 +0000

    Initial App

---
+++ DeskFit/Exercises.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeskFit
+{
+    public class Exercises
+    {
+        List<string> exercises = new List<string>
+        {
+            "Get a drink",
+            "Run up and down the stairs twice",
+            "Stretch",
+            "10 crunches",
+            "10 situps",
+            "10 squats",
+            "20 walking lunges",
+            "10s plank",
+            "10 jumping jacks"
+        };
+
+        static Random rand = new Random();
+
+        public string GetExercise()
+        {
+            int i = rand.Next(exercises.Count);
+
+            return(exercises[i]);
+
+        }
+    }
+}
--- DeskFit/MainPage.xaml.cs
+++ DeskFit/MainPage.xaml.cs
@@ -21,46 +21,134 @@ namespace DeskFit
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+
     public sealed partial class MainPage : Page
     {
-
         DispatcherTimer timer;
 
-        TimeSpan breakTime = new TimeSpan(0, 0, 5);
+        TimeSpan breakInterval = new TimeSpan(0, 30, 0);
+        TimeSpan breakTime = new TimeSpan(0, 5, 0);
 
         TimeSpan timeLeft;
+
+        // make 2 state variables so we can tell what we are doing
+        String state = "countdown";
+        String countdownType = "toBreak";
+
+        // make it so that the state variable can be monitored for debugging
+        public String State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                Debug.WriteLine("State: " + state);
+            }
+        }
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            timeLeft = breakTime;
+            // reset the timer to the break time
+            timeLeft = breakInterval;
 
+            // create a new timer
             timer = new DispatcherTimer();
+            // set the timer function
             timer.Tick += Timer_Tick;
+            // set the interval of the timer to 1 second
             timer.Interval = new TimeSpan(0, 0, 1);
+            // start the timer
             timer.Start();
+
+            Progress.Maximum = breakInterval.TotalSeconds;
         }
         
         private void Timer_Tick(object sender, object e)
         {
+            // take away 1 second from the time left
             timeLeft = timeLeft.Subtract(new TimeSpan(0, 0, 1));
-            header.Text = timeLeft.ToString(@"mm\:ss");
 
-            if (timeLeft <= new TimeSpan(0, 0, 0))
+            // if the time has not run out
+            if (timeLeft >= new TimeSpan(0, 0, 0))
             {
-                Debug.WriteLine("Time is Up!");
-                //mainGrid.Background = Application.Current.Resources["ApplicationPageBackgroundThemeBrush"] as Brush;
-                timer.Stop();
+                // display the time on the screen
+                Header.Text = timeLeft.ToString(@"mm\:ss");
+
+                // update the progress bar
+                Progress.Value = timeLeft.TotalSeconds;
+            }
+
+            // if we are counting down to a break
+            if (countdownType == "toBreak")
+            {
+
+                // check if the time has run out
+                if (timeLeft < new TimeSpan(0, 0, 0))
+                {
+                    // change the text on the button
+                    ActionButton.Content = "I'm Ready!";
+                    
+                    // flash the screen
+                    // if the screen was red
+                    if (State == "flash:red")
+                    {
+                        // change the background to black
+                        MainGrid.Background = Application.Current.Resources["ApplicationPageBackgroundThemeBrush"] as Brush;
+                        State = "flash:black";
+                    }
+                    // if the screen was not red (so black)
+                    else
+                    {
+                        // change the background to red
+                        MainGrid.Background = (SolidColorBrush)Resources["DangerRed"];
+                        State = "flash:red";
+                    }
+                }
+            }
+            else if (countdownType == "breakCountdown")
+            {
+                // check if the time has run out
+                if (timeLeft < new TimeSpan(0, 0, 0))
+                {
+                    EndBreak();
+                }
             }
         }
 
-        private void mainGrid_KeyUp(object sender, KeyRoutedEventArgs e)
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
         {
-            if (e.Key == Windows.System.VirtualKey.Enter)
+            if (countdownType == "toBreak")
             {
-                Debug.WriteLine("Press");
+                BeginNextBreak();
+            } else if (countdownType == "breakCountdown")
+            {
+                EndBreak();
             }
+        }
+
+        void BeginNextBreak()
+        {
+            var exercises = new Exercises();
+
+            SubHeader.Text = exercises.GetExercise();
+            countdownType = "breakCountdown";
+            timeLeft = breakTime;
+            Progress.Maximum = breakTime.TotalSeconds;
+            ActionButton.Content = "Finish";
+            MainGrid.Background = (SolidColorBrush)Resources["ExerciseBlue"];
+        }
+
+        void EndBreak()
+        {
+            SubHeader.Text = "Time to Next Break";
+            countdownType = "toBreak";
+            timeLeft = breakInterval;
+            Progress.Maximum = breakInterval.TotalSeconds;
+            ActionButton.Content = "Break Now";
+            MainGrid.Background = Application.Current.Resources["ApplicationPageBackgroundThemeBrush"] as Brush;
         }
     }
 }
